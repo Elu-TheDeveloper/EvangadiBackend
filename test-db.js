315a126c2c4194053c1b3bbd7b179cbdb206ec6f
EluTheDeveloper <eladaba2022@gmail.com>
2025-05-25 11:11:27 +0300

    addingdbport

--- test-db.js
+++ test-db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql2 = require('mysql2');
 
 const dbConnection = mysql2.createPool({
-  host: process.env.DB_HOST,     
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -10,4 +10,12 @@ const dbConnection = mysql2.createPool({
   connectionLimit: 10
 });
 
-module.exports = dbConnection.promise();
+dbConnection.promise().execute("SELECT 1")
+  .then(([rows]) => {
+    console.log('DB connection success:', rows);
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error('DB connection error:', err.message);
+    process.exit(1);
+  });
